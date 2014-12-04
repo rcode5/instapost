d661f089ade82343d9d2a3d9e8370f36c7c06a94
Mr Rogers <jon@bunnymatic.com>
2014-12-04 08:57:24 -0800

    More trimming and styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
+  PICS_PER_PAGE = 20
   def index
-    @posts = Post.all.order created_at: :desc
+    @posts = (Post.where(:created_at > params[:s]).order created_at: :desc).limit(PICS_PER_PAGE)
   end
 end
